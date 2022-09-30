cc5499c533f7c26b43b2323ecf1471359e644277
robertodr01 <roberto@dellarocca.org>
2022-09-30 12:49:44 +0200

    testing pull request

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -3,6 +3,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Test Github Action");
+        System.out.println("Testing Pull Request");
     }
 
 }
